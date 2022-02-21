1c80f2d41cf1f04f145c0ca735c408c51b80e539
20toduc01 <20toduc01@gmail.com>
2022-02-21 17:19:22 +0700

    Stochastic depth

--- models/common.py
+++ models/common.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, depthwise: bool = False) -> nn.Conv2d:
@@ -10,3 +12,21 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, depthwise: bool = 
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
     """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class StochasticModule(nn.Module):
+    """
+    Stochastic Depth: https://arxiv.org/abs/1603.09382
+    Randomly drop layers in each forward pass for the whole input batch
+    """
+    def __init__(
+        self,
+        module: nn.Module,
+        survival_rate: float = 1.
+    ) -> None:
+        super().__init__()
+        self.module = module
+        self._drop = torch.distributions.Bernoulli(torch.tensor(1 - survival_rate))
+    
+    def forward(self, x: Tensor) -> Tensor:
+        return 0 if self.training and self._drop.sample() else self.module(x)
