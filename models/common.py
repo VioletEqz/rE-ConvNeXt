cca2bfabd4c3d207b32baa6d567d4d2161e74417
20toduc01 <20toduc01@gmail.com>
2022-02-25 14:48:37 +0700

    Move d3x3 up and use larger kernel

--- models/common.py
+++ models/common.py
@@ -15,6 +15,10 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, depthwise: bool = 
     groups = in_planes if depthwise else 1
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, groups=groups, bias=bias)
 
+def conv7x7(in_planes: int, out_planes: int, stride: int = 1, depthwise: bool = False, bias: bool = False) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    groups = in_planes if depthwise else 1
+    return nn.Conv2d(in_planes, out_planes, kernel_size=7, stride=stride, padding=3, groups=groups, bias=bias)
 
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
     """1x1 convolution"""
