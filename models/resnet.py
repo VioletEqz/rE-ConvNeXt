b479e862faa1981197a34e7b92c6c7f044079b7a
20toduc01 <20toduc01@gmail.com>
2022-02-28 14:12:25 +0700

    Minor

--- models/resnet.py
+++ models/resnet.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from torch import Tensor
 
-from common import benchmark, conv3x3, conv1x1
+from .common import benchmark, conv3x3, conv1x1
 
 class BasicBlock(nn.Module):
     expansion: int = 1
